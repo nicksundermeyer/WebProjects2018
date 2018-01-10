054ab6e8cd8e6a499b99c2ecbdfbd12fe29f3cfa
Evan Hicks <hicks2evan@gmail.com>
2018-01-09 20:50:58 -0700

    teacher angular setup

--- client/app/app.js
+++ client/app/app.js
@@ -22,13 +22,14 @@ import navbar from '../components/navbar/navbar.component';
 import footer from '../components/footer/footer.component';
 import main from './main/main.component';
 import student from './student/student.component';
+import teacher from './teacher/teacher.component';
 import constants from './app.constants';
 import util from '../components/util/util.module';
 
 import './app.scss';
 
 angular.module('webProjectsApp', [ngCookies, ngResource, ngSanitize, ngRoute, uiBootstrap, _Auth,
-  account, admin, 'validation.match', navbar, footer, main, constants, util, student
+  account, admin, 'validation.match', navbar, footer, main, constants, util, student, teacher
 ])
   .config(routeConfig)
   .run(function($rootScope, $location, Auth) {
