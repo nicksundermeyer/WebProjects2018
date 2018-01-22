b34cd40dcd06411fa28b07e858320cf0e347906e
Justin Martz <jmartz@Justins-MacBook-Pro.local>
2018-01-21 18:41:18 -0700

    Added categories and subject to shared.js

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -2,5 +2,9 @@
 
 exports = module.exports = {
   // List of user roles
-  userRoles: ['guest', 'user', 'student', 'teacher', 'researcher', 'admin']
+  userRoles: ['guest', 'user', 'student', 'teacher', 'researcher', 'admin'],
+
+  //categories and subject are organized by index, that is that subject[i] has corresponding categories at categories[i]
+  subject: ['booleanLogic', 'algebra'],
+  categories: [['or', 'and', 'not', 'implication'], ['addition', 'subtraction', 'multiplication', 'division', 'linearEquations', 'quadraticEquations']]
 };
