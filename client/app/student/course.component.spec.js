9f4bf6bd95942928c4d6cda29cd3aa7fefb40f75
Evan Hicks <hicks2evan@gmail.com>
2018-01-24 11:48:25 -0700

    adding back to course page button to assignment page

--- client/app/student/course.component.spec.js
+++ client/app/student/course.component.spec.js
@@ -15,8 +15,6 @@ describe('Component: CourseComponent', function() {
   // Initialize the controller and a mock scope
   beforeEach(inject(function(_$httpBackend_, $http, $componentController, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
     scope = $rootScope.$new();
     courseComponent = $componentController('course', {
