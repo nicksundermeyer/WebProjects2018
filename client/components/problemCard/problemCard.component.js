27fce967933053c74378fa69ae6107ac3596d484
Evan Hicks <hicks2evan@gmail.com>
2018-02-01 00:30:18 -0700

    Working on complex math entry. Packaged mathlex as node module and installed. Installed katex latex display library. Made changes to problem card as a sample of how input binding and updating could work.

--- client/components/problemCard/problemCard.component.js
+++ client/components/problemCard/problemCard.component.js
@@ -1,15 +1,33 @@
 'use strict';
 
 import angular from 'angular';
+import 'mathlex';
+import katex from 'katex';
 
 export class ProblemCardComponent {
+
+  userInput;
+  ast;
+  latex;
+
   /*@ngInject*/
   constructor($location) {
     'ngInject';
     this.$location = $location;
+
+    this.userInput = '';
+    this.ast = '';
+    this.latex = '';
+  }
+
+  updateDisplay() {
+    this.ast = MathLex.parse(this.userInput);
+    this.latex = MathLex.render(this.ast, 'latex');
+    katex.render(this.latex, document.getElementById('display'));
   }
 
 }
+
 export default angular.module('directives.problemCard', [])
   .component('problemCard', {
     template: require('./problemCard.html'),
