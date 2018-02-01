1a3e15a1a472dd7b554ae36344fa5d41f28bd7e6
Daniel Pittman <daniel@pittmo.net>
2018-01-31 18:49:08 -0700

    Updated shared.js to restrict categories, modified model and seed to use new shared.js

--- server/api/course/TailoredCourse.model.js
+++ server/api/course/TailoredCourse.model.js
@@ -8,20 +8,20 @@ import shared from './../../config/environment/shared';
 var TailoredCourseSchema = new Schema({
   //grabs information from abstract course
   //such as name and description
-  abstractCourseID: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'AbstractCourse' 
+  abstractCourseID: {
+    type: Schema.Types.ObjectId,
+    ref: 'AbstractCourse'
   },
 
   //identifies a student enrolled in this course
-  studentID: { 
-    type: Schema.Types.ObjectId, 
+  studentID: {
+    type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  
+
   //subjects and categories must match the enums defined below
-  subjects: String, enum: shared.subject,
-  categories: String, enum: shared.categories,
+  subjects: String, enum: shared.allSubjects,
+  categories: String, enum: shared.allCategories,
 
   //grabs the assignments written for this course
   assignments: [Assignment.schema]
--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -1,13 +1,54 @@
 'use strict';
 
+let subjects = [{
+  subject: 'booleanLogic',
+  allowedCategories: [
+    'or',
+    'and',
+    'not',
+    'implication'
+  ]
+}, {
+  subject: 'algebra',
+  allowedCategories: [
+    'addition',
+    'subtraction',
+    'multiplication',
+    'division',
+    'linearEquations',
+    'quadraticEquations'
+  ]
+}];
+
+let allCategories = [];
+let allSubjects = [];
+subjects.forEach(function (subject) {
+  allCategories.concat(subject.allowedCategories);
+  allSubjects.push(subject.subject);
+});
+
+allCategories = arrayUnique(allCategories);
+
 exports = module.exports = {
-
-
   problemEngineUrl: 'https://problem-engine.herokuapp.com/problems',
   // List of user roles
   userRoles: ['guest', 'user', 'student', 'teacher', 'researcher', 'admin'],
 
-  //categories and subject are organized by index, that is that subject[i] has corresponding categories at categories[i]
-  subjects: ['booleanLogic', 'algebra'],
-  categories: [['or', 'and', 'not', 'implication'], ['addition', 'subtraction', 'multiplication', 'division', 'linearEquations', 'quadraticEquations']]
+  subjects: subjects,
+  allCategories: allCategories,
+  allSubjects: allSubjects
 };
+
+// https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
+function arrayUnique(array) {
+  var a = array.concat();
+  for(var i = 0; i < a.length; ++i) {
+    for(var j = i + 1; j < a.length; ++j) {
+      if(a[i] === a[j])
+        a.splice(j--, 1);
+    }
+  }
+
+  return a;
+}
+
