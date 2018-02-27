b24818093bc74a4871df2b83c1e80129dc96feb1
Justin Martz <jmartz@Justins-MacBook-Pro.local>
2018-02-26 17:43:34 -0700

    dont want to return problems from tc at all

--- server/api/course/course.controller.js
+++ server/api/course/course.controller.js
@@ -157,7 +157,7 @@ export function getTailoredCourse(req, res) {
     .populate({path: 'abstractCourseID', select: 'name description -_id'})
     .populate({
       path: 'assignments',
-      select: '-problems.problem.solution',
+      select: '-problems',
       populate: {
         path: 'AbstractAssignmentId',
         model: 'AbstractAssignment',
