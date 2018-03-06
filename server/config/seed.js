69ca54d09765b6c76f038a5b93f406d3b567e028
Simao Nziaka <sdnziaka@gmail.com>
2018-03-05 18:01:28 -0700

    thing controller gone seed now has a static problem

--- server/config/seed.js
+++ server/config/seed.js
@@ -4,7 +4,6 @@
  */
 
 'use strict';
-import Thing from '../api/thing/thing.model';
 import User from '../api/user/user.model';
 import AbstractCourse from '../api/course/abstractCourse.model';
 import TailoredCourse from '../api/course/tailoredCourse.model';
@@ -17,43 +16,6 @@ import * as problemController from '../api/problem/problem.controller';
 
 export default function seedDatabaseIfNeeded() {
   if(config.seedDB) {
-    //not related to app data - could be deleted
-    Thing.find({}).remove()
-    .then(() => {
-      let thing = Thing.create({
-        name: 'Development Tools',
-        info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-        + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-        + 'Stylus, Sass, and Less.'
-      }, {
-        name: 'Server and Client integration',
-        info: 'Built with a powerful and fun stack: MongoDB, Express, '
-        + 'AngularJS, and Node.'
-      }, {
-        name: 'Smart Build System',
-        info: 'Build system ignores `spec` files, allowing you to keep '
-        + 'tests alongside code. Automatic injection of scripts and '
-        + 'styles into your index.html'
-      }, {
-        name: 'Modular Structure',
-        info: 'Best practice client and server structures allow for more '
-        + 'code reusability and maximum scalability'
-      }, {
-        name: 'Optimized Build',
-        info: 'Build process packs up your templates as a single JavaScript '
-        + 'payload, minifies your scripts/css/images, and rewrites asset '
-        + 'names for caching.'
-      }, {
-        name: 'Deployment Ready',
-        info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-        + 'and openshift subgenerators'
-      });
-      return thing;
-    })
-    .then(() => console.log('finished populating things'))
-    .catch(err => console.log('error populating things', err));
-    //end Things
-
     //for every role on shared user roles, create a user for that role.
     for(let role of shared.userRoles) {
       User.find({}).remove()
