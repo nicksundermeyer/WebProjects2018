d27bd35660c585710e9894a8fb1db0980aa3ab4e
Anh Tran <superlem95@yahoo.com>
2018-03-14 18:08:37 -0600

    half working create integration

--- server/api/course/course.integration.js
+++ server/api/course/course.integration.js
@@ -1,23 +1,22 @@
 'use strict';
 
-/* globals describe, expect, it, beforeEach, afterEach */
+/* globals describe, expect, it, beforeEach, afterEach, before */
 
 import User from '../user/user.model';
 
 var app = require('../..');
 import request from 'supertest';
 
-
-
 describe('Course API:', function() {
   var newCourse;
   var token;
+
   //login
-  describe('Login /auth/local', function(){
+  describe('Login /auth/local', function() {
     var user;
     // Clear users before testing
     before(function() {
-      return User.remove().then(function () {
+      return User.remove().then(function() {
         user = new User({
           name: 'Fake Teacher',
           email: 'teacher@example.com',
@@ -29,7 +28,7 @@ describe('Course API:', function() {
       });
     });
 
-    before(function (done) {
+    before(function(done) {
       request(app)
         .post('/auth/local')
         .send({
@@ -44,16 +43,16 @@ describe('Course API:', function() {
         });
     });
 
-    it('should validate token exists', function () {
+    it('should validate token exists', function() {
       expect(token).to.not.be.an('undefined');
     });
   });
 
   //show all courses
-  describe('GET api/courses', function () {
+  describe('GET api/courses', function() {
     var getCourses;
 
-    beforeEach(function (done) {
+    beforeEach(function(done) {
       request(app)
         .get('/api/courses')
         .expect(200)
@@ -67,10 +66,52 @@ describe('Course API:', function() {
         });
     });
 
-    it('should respond with a json array', function () {
+    it('should respond with a json array', function() {
       expect(getCourses).to.be.instanceOf(Array);
     });
   });
 
+  //create a course if a teacher
+  describe('POST api/courses', function() {
+    beforeEach(function(done) {
+      request(app)
+        .post('/api/courses')
+        .set('authorization', `Bearer ${token}`)
+        .send({
+          name: 'survival class',
+          description: 'how to make fire',
+          subjects: ['booleanLogic'],
+          categories: ['or'],
+          assignment: [{
+            title: 'assignment 1',
+            description: 'find trees',
+            minNumProblems: 5,
+            maxNumProblems: 10,
+            newProblemPercentage: 17
+          }]
+        })
+        .expect(201)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .end((err, res) => {
+          if(err) {
+            console.log('Bearer ' + token);
+            return done(err);
+          }
+          newCourse = res.body;
+          done();
+        });
+    });
+    it('should respond with the newly created thing', function() {
+      expect(newCourse.name).to.equal('survival class');
+      expect(newCourse.description).to.equal('how to make fire');
+      expect(newCourse.subjects).to.equal('booleanLogic');
+      expect(newCourse.categories).to.equal('or');
+      //expect(newCourse.assignments[0].title).to.equal('assignment 1');
+      // expect(newCourse.assignment.description).to.equal('find trees');
+      // expect(newCourse.assignment.minNumProblems).to.equal(5);
+      // expect(newCourse.assignment.maxNumProblems).to.equal(10);
+      // expect(newCourse.assignment.newProblemPercentage).to.equal(17);
+    });
+  });
 });
 
