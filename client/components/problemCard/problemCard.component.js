5f8a260880f405247280931cc9f0142494c8c8a2
Dalton Crutchfield <dalton.crutchfield@du.edu>
2018-05-09 18:31:56 -0600

    autonavigate issue solved

--- client/components/problemCard/problemCard.component.js
+++ client/components/problemCard/problemCard.component.js
@@ -180,7 +180,7 @@ export class ProblemCardComponent {
   addAlert(type, msg) {
     console.log('added alert');
     this.alerts.push({ type, msg });
-    if (msg == 'Correct') {
+    if (msg == 'Correct!') {
       this.isCorrect = true;
     } else {
       this.isCorrect = false;
