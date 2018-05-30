3e18c84a52057b150bd1d5adf1298dc6c82baf16
Dalton Crutchfield <dalton.crutchfield@du.edu>
2018-05-30 17:58:34 -0600

    visualization attempt stuff

--- client/app/teacher/teacher.component.js
+++ client/app/teacher/teacher.component.js
@@ -1,15 +1,19 @@
 import angular from 'angular';
 const ngRoute = require('angular-route');
 const uiBootstrap = require('angular-ui-bootstrap');
+//const uiNvd3 = require('nvd3');
 import routing from './teacher.routes';
 import gravatar from 'gravatar';
 import auth from '../../services/auth/auth.module';
 import user from '../../services/user/user.module';
+import d3 from 'd3';
 
 export class TeacherController {
   courses = [];
   teacher;
   gravatarUrl;
+  options;
+  data;
 
   /*@ngInject*/
   constructor($http, $uibModal, Auth, UserServ) {
@@ -48,6 +52,53 @@ export class TeacherController {
         });
       });
     });
+
+    this.options = JSON.stringify({
+      chart: {
+        type: 'discreteBarChart',
+        height: 450,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 60,
+          left: 55
+        },
+        x: function(d) {
+          return d.label;
+        },
+        y: function(d) {
+          return d.value;
+        },
+        showValues: true,
+        valueFormat: function(d) {
+          return d3.format(',.4f')(d);
+        },
+        transitionDuration: 500,
+        xAxis: {
+          axisLabel: 'X Axis'
+        },
+        yAxis: {
+          axisLabel: 'Y Axis',
+          axisLabelDistance: 30
+        }
+      }
+    });
+
+    this.data = JSON.stringify([
+      {
+        key: 'Cumulative Return',
+        values: [
+          { label: 'A', value: -29.765957771107 },
+          { label: 'B', value: 0 },
+          { label: 'C', value: 32.807804682612 },
+          { label: 'D', value: 196.45946739256 },
+          { label: 'E', value: 0.19434030906893 },
+          { label: 'F', value: -98.079782601442 },
+          { label: 'G', value: -13.925743130903 },
+          { label: 'H', value: -5.1387322875705 }
+        ]
+      }
+    ]);
   }
 
   // open modal
