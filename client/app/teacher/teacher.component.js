e8e8d58489d11689028ef07ef6654e10a949e4e1
Dalton Crutchfield <dalton.crutchfield@du.edu>
2018-05-30 19:39:47 -0600

    uhhhhhh

--- client/app/teacher/teacher.component.js
+++ client/app/teacher/teacher.component.js
@@ -54,52 +54,56 @@ export class TeacherController {
       });
     });
 
-    this.options = JSON.stringify({
-      chart: {
-        type: 'discreteBarChart',
-        height: 450,
-        margin: {
-          top: 20,
-          right: 20,
-          bottom: 60,
-          left: 55
-        },
-        x: function(d) {
-          return d.label;
-        },
-        y: function(d) {
-          return d.value;
-        },
-        showValues: true,
-        valueFormat: function(d) {
-          return d3.format(',.4f')(d);
-        },
-        transitionDuration: 500,
-        xAxis: {
-          axisLabel: 'X Axis'
-        },
-        yAxis: {
-          axisLabel: 'Y Axis',
-          axisLabelDistance: 30
+    this.options = JSON.parse(
+      JSON.stringify({
+        chart: {
+          type: 'discreteBarChart',
+          height: 450,
+          margin: {
+            top: 20,
+            right: 20,
+            bottom: 60,
+            left: 55
+          },
+          x: function(d) {
+            return d.label;
+          },
+          y: function(d) {
+            return d.value;
+          },
+          showValues: true,
+          valueFormat: function(d) {
+            return d3.format(',.4f')(d);
+          },
+          transitionDuration: 500,
+          xAxis: {
+            axisLabel: 'X Axis'
+          },
+          yAxis: {
+            axisLabel: 'Y Axis',
+            axisLabelDistance: 30
+          }
         }
-      }
-    });
+      })
+    );
 
-    this.data = JSON.stringify([
-      {
-        key: 'Cumulative Return',
-        values: [
-          { label: 'A', value: -29.765957771107 },
-          { label: 'B', value: 0 },
-          { label: 'C', value: 32.807804682612 },
-          { label: 'D', value: 196.45946739256 },
-          { label: 'E', value: 0.19434030906893 },
-          { label: 'F', value: -98.079782601442 },
-          { label: 'G', value: -13.925743130903 },
-          { label: 'H', value: -5.1387322875705 }
-        ]
-      }
-    ]);
+    this.data = JSON.parse(
+      JSON.stringify([
+        {
+          key: 'Cumulative Return',
+          values: [
+            { label: 'A', value: -29.765957771107 },
+            { label: 'B', value: 0 },
+            { label: 'C', value: 32.807804682612 },
+            { label: 'D', value: 196.45946739256 },
+            { label: 'E', value: 0.19434030906893 },
+            { label: 'F', value: -98.079782601442 },
+            { label: 'G', value: -13.925743130903 },
+            { label: 'H', value: -5.1387322875705 }
+          ]
+        }
+      ])
+    );
   }
 
   // open modal
@@ -112,7 +116,7 @@ export class TeacherController {
 }
 
 export default angular
-  .module('webProjectsApp.teacher', [ngRoute, uiBootstrap, uinvd3, auth, user])
+  .module('webProjectsApp.teacher', [ngRoute, uiBootstrap, auth, user])
   .config(routing)
   .component('teacher', {
     template: require('./teacher.html'),
