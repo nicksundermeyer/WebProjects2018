f48aedc877fc47146d48e98c497f51f94f38bf77
Dalton Crutchfield <dalton.crutchfield@du.edu>
2018-06-03 12:49:40 -0600

    Auto stash before merge of "Visualization" and "upstream/Visualization"
    Pie Chart

--- client/app/teacher/teacher.component.js
+++ client/app/teacher/teacher.component.js
@@ -58,49 +58,57 @@ export class TeacherController {
 
     this.options = {
       chart: {
-        type: 'discreteBarChart',
-        height: 450,
-        margin: {
-          top: 20,
-          right: 20,
-          bottom: 60,
-          left: 55
-        },
+        type: 'pieChart',
+        height: 500,
         x: function(d) {
-          return d.label;
+          return d.key;
         },
         y: function(d) {
-          return d.value;
+          return d.y;
         },
-        showValues: true,
-        valueFormat: function(d) {
-          console.log(d);
-          return d3.format(',.4f')(d);
-        },
-        transitionDuration: 500,
-        xAxis: {
-          axisLabel: 'X Axis'
-        },
-        yAxis: {
-          axisLabel: 'Y Axis',
-          axisLabelDistance: 30
+        showLabels: true,
+        duration: 500,
+        labelThreshold: 0.01,
+        labelSunbeamLayout: true,
+        legend: {
+          margin: {
+            top: 5,
+            right: 35,
+            bottom: 5,
+            left: 0
+          }
         }
       }
     };
 
     this.data = [
       {
-        key: 'Cumulative Return',
-        values: [
-          { label: 'A', value: -29.765957771107 },
-          { label: 'B', value: 0 },
-          { label: 'C', value: 32.807804682612 },
-          { label: 'D', value: 196.45946739256 },
-          { label: 'E', value: 0.19434030906893 },
-          { label: 'F', value: -98.079782601442 },
-          { label: 'G', value: -13.925743130903 },
-          { label: 'H', value: -5.1387322875705 }
-        ]
+        key: 'One',
+        y: 5
+      },
+      {
+        key: 'Two',
+        y: 2
+      },
+      {
+        key: 'Three',
+        y: 9
+      },
+      {
+        key: 'Four',
+        y: 7
+      },
+      {
+        key: 'Five',
+        y: 4
+      },
+      {
+        key: 'Six',
+        y: 3
+      },
+      {
+        key: 'Seven',
+        y: 0.5
       }
     ];
   }
