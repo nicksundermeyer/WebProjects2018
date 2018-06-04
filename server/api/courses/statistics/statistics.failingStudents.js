ab2830e309211f1549a0e97b49f2069eea62c788
Andres Miguel Espineira <amespineira@me.com>
2018-06-04 15:00:06 -0600

    rewrote failing students calculator to use promise chaining instead of map reduce, currently generates array of all students in course with grades

--- server/api/courses/statistics/statistics.failingStudents.js
+++ server/api/courses/statistics/statistics.failingStudents.js
@@ -3,75 +3,71 @@
 import mongoose, { Schema } from 'mongoose';
 import shared from './../../../config/environment/shared';
 import TailoredCourse from './../tailoredCourses/tailoredCourse.model';
-console.log('here in failing students');
+import AbstractCourse from '../abstractCourses/abstractCourse.model';
+import TailoredAssignment from '../tailoredCourses/tailoredAssignment.model';
+import Submission from '../submission/submission.model';
 
 module.exports = function failingStudents(req) {
-  //
-  //
-  //map
-  //go through students
-  //if a student: in the course, and failing
-  //emit students name and grade
+  return TailoredCourse.find({
+    abstractCourseID: mongoose.Types.ObjectId(req.params.id)
+  }).then(function(tailoredCourses) {
+    var assignmentsPromise = [];
+    var assignmentsOutput = {};
+    tailoredCourses.forEach(function(course) {
+      course.assignments.forEach(function(assignment) {
+        assignmentsOutput[assignment.valueOf()] = {
+          studentId: course.studentID,
+          totalFail: 0,
+          totalPass: 0
+        };
+        assignmentsPromise.push(TailoredAssignment.find({ _id: assignment }));
+      });
+    });
 
-  //calculating failing:
-  //
-
-  // start with asbtract course,
-  //     go through every associated tailored course
-  //         calculate grade
-  //             if
-  //
-  //
-  //           look at all tailored courses, only look at ones
-  //           take lates attempt
-  //
-  //           map: emit student id and multiple values for them
-  //               one per problem, binary pass fail
-  var o = {};
-  o.scope = {
-    target: req.params.id
-  };
-  o.map = function() {
-    var numFail = 0;
-    var numPass = 0;
-    if (this.abstractCourseID.valueOf() == target) {
-      if (this.assignments) {
-        this.assignments.forEach(function(assignment) {
-          if (assignment.problems) {
-            assignment.problems.forEach(function(problem) {
-              if (problem.attempts && problem.attempts.length > 0) {
-                if (problem.attempts[problem.attempts.length - 1].correct) {
-                  numPass++;
-                } else {
-                  numFail++;
-                }
-              }
-            });
+    return Promise.all(assignmentsPromise).then(function(assignments) {
+      assignments.forEach(function(assignment) {
+        assignment[0].problems.forEach(function(problem) {
+          console.log(problem);
+          if (problem.attempts.length > 0) {
+            if (problem.attempts[problem.attempts.length - 1].correct) {
+              assignmentsOutput[assignment[0]._id.valueOf()].totalPass++;
+            } else {
+              assignmentsOutput[assignment[0]._id.valueOf()].totalFail++;
+            }
           }
         });
+      });
+
+      var students = {};
+      for (var assignmentOut in assignmentsOutput) {
+        if (!students[assignmentsOutput[assignmentOut].studentId]) {
+          students[assignmentsOutput[assignmentOut].studentId] = {
+            totalPass: assignmentsOutput[assignmentOut].totalPass,
+            totalFail: assignmentsOutput[assignmentOut].totalFail
+          };
+        } else {
+          students[assignmentsOutput[assignmentOut].studentId].totalPass +=
+            assignmentsOutput[assignmentOut].totalPass;
+          students[assignmentsOutput[assignmentOut].studentId].totalFail +=
+            assignmentsOutput[assignmentOut].totalFail;
+        }
       }
-      emit(this.studentID, { numPass, numFail });
-    }
-  };
 
-  o.reduce = function(k, vals) {
-    var totalPass = 0;
-    var totalFail = 0;
-    vals.forEach(function(val) {
-      totalPass += val.numPass;
-      totalFail += val.numFail;
+      var output = [];
+      for (var student in students) {
+        var percent =
+          students[student].totalPass /
+          (students[student].totalPass + students[student].totalFail);
+        output.push({
+          studentId: student,
+          percentage: percent
+        });
+      }
+
+      return Promise.resolve({
+        description: 'returns the failing students in a course',
+        students: output
+      });
     });
-    // Calculate their overall grade
-    var percentage = totalPass / (totalPass + totalFail);
-    return {
-      percentage: percentage,
-      numPass: totalPass,
-      numFail: totalFail
-    };
-  };
-
-  return TailoredCourse.mapReduce(o).then(function(results) {
-    console.log(results);
-    return results;
   });
 };
